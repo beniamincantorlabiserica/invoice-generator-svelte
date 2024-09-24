b87f78977d8967afffe0d9759b44d8d5003dd679
beniamin <avr.beniamin@icloud.com>
2024-09-24 19:38:18 +0200

    added images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { goto } from '$app/navigation';
+	import Professional from '$lib/templates/professional.png';
+	import Modern from '$lib/templates/modern.png';
+	import Simple from '$lib/templates/simple.png';
 
 	const templates = [
-		{ id: 1, name: 'Simple', image: '/templates/simple.png' },
-		{ id: 2, name: 'Professional', image: '/templates/professional.png' },
-		{ id: 3, name: 'Modern', image: '/templates/modern.png' },
+		{ id: 1, name: 'Simple', image: Simple },
+		{ id: 2, name: 'Professional', image: Professional },
+		{ id: 3, name: 'Modern', image: Modern },
 	];
 
 	function selectTemplate(id) {
@@ -17,7 +20,7 @@
 <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
 	{#each templates as template}
 		<div class="card bg-base-100 shadow-xl">
-			<figure><img src={template.image} alt={template.name} /></figure>
+			<figure><img src={template.image} alt={template.name} class="w-60 h-48" /></figure>
 			<div class="card-body">
 				<h3 class="card-title">{template.name}</h3>
 				<div class="card-actions justify-end">
